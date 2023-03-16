5c5920dedbcbf5fd1c0bf45826b90541f5ac7244
洮羱芝闇 <shun_@outlook.com>
2023-03-16 11:02:22 +0800

    update

--- 3.无重复字符的最长子串/NoRe.java
+++ 3.无重复字符的最长子串/NoRe.java
@@ -1,5 +1,23 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        
+        int long1 = 0;
+        LinkedList<String> list = new LinkedList<>();
+        String[] ones = s.split("");
+        for(String one:ones){
+            if(!list.contains(one)&&!one.equals("")){
+                list.add(one);
+                if(list.size()>long1){
+                    long1 = list.size();
+                }
+            }else{
+                if(list.size()>0){
+                    while(!list.poll().equals(one)){
+                        
+                    }
+                    list.add(one);
+                }
+            }
+        }
+        return long1;
     }
 }
