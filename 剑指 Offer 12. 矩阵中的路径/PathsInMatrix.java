e9ebdbbbe8fa93173cf1173144b26857df93bd6f
洮羱芝闇 <shun_@outlook.com>
2023-08-04 15:47:25 +0800

    必须使用纯正的回溯

--- 剑指 Offer 12. 矩阵中的路径/PathsInMatrix.java
+++ 剑指 Offer 12. 矩阵中的路径/PathsInMatrix.java
@@ -1,3 +1,23 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println(new Solution().exist(new char[][]{{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}}, "ABCB"));
+        //System.out.println(new Solution().exist(new char[][]{{'a','b'}}, "ba"));
+        //System.out.println(new Solution().exist(new char[][]{{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}}, "SEE"));
+        //["A","B","C","E"],["S","F","E","S"],["A","D","E","E"]
+        System.out.println(new Solution().exist(new char[][]{{'A', 'B', 'C', 'E'}, {'S', 'F', 'E', 'S'}, {'A', 'D', 'E', 'E'}}, "ABCESEEEFS"));
+        //[["A","B","C","E"],["S","F","E","S"],["A","D","E","E"]]
+        //System.out.println(new Solution().exist(new char[][]{{'A', 'B', 'C', 'E'}, {'S', 'F', 'E', 'S'}, {'A', 'D', 'E', 'E'}}, "ABCEFSADEESE"));
+        /*Solution.Way way = new Solution.Way();
+        way.setWay(1,1,2);
+        System.out.println(way.getWay(1,1));*/
+    }
+}
+
 class Solution {
     public boolean exist(char[][] board, String word) {
         if (board.length == 0 || board[0].length == 0) {
@@ -8,8 +28,8 @@ class Solution {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                 if (split[0].equals(String.valueOf(board[i][j]))) {
-                    boolean test = test(board,i, j,split);
-                    if(test == true){
+                    boolean test = test(board, i, j, split,1);
+                    if (test == true) {
                         return true;
                     }
                 }
@@ -18,38 +38,23 @@ class Solution {
         return false;
     }
 
-    public boolean test(char[][] board,int x,int y,String[] split){
-        int now = 1;
-        while (now < board.length) {
-            boolean flag1 = false;
-            //检查上面
-            if((x-1>0)&&split[now].equals(String.valueOf(board[x-1][y]))){
-                flag1 = true;
-                x = x-1;
-                now++;
-            }
-            //检查下面
-            else if((x+1<board.length)&&split[now].equals(String.valueOf(board[x+1][y]))){
-                flag1 = true;
-                x = x+1;
-                now++;
-            }
-            //检查左面
-            else if((y-1>0)&&split[now].equals(String.valueOf(board[x][y-1]))){
-                flag1 = true;
-                y = y-1;
-                now++;
-            }
-            //检查右面
-            else if((y+1<board[0].length)&&split[now].equals(String.valueOf(board[x][y+1]))){
-                flag1 = true;
-                y = y+1;
-                now++;
-            }
-            if(!flag1){
-                return false;
+    public boolean test(char[][] board,int x,int y,String[] split, int now){
+        if(now == split.length) return true;
+        char temp = board[x][y];
+        board[x][y] = ' ';
+        boolean res = false;
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        for(int i=0; i<4; i++){
+            int newx = x + dx[i];
+            int newy = y + dy[i];
+            if(newx >= 0 && newx < board.length && newy >= 0 && newy < board[0].length && split[now].equals(String.valueOf(board[newx][newy]))){
+                res = res || test(board,newx,newy,split,now+1);
             }
         }
-        return true;
+        board[x][y] = temp;
+        return res;
     }
 }
+
+//必须使用真正的回溯
